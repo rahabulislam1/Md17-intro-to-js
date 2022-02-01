e8a46d0e0f5cd1ce43cce6ecc28d1cd910020529
rahabulislam1 <rahabulislam1@gmail.com>
2022-02-01 12:12:24 +0600

    loop practice

---
+++ loop.js
@@ -0,0 +1,19 @@
+var i = 1, j;
+
+// while (i <= 10) {
+//     console.log('Count Num: ')
+//     console.log(i);
+//     i++;
+// }
+//even Number
+while (i <= 20) {
+    if (i % 2 == 0) {
+        console.log(i);
+    }
+    i++;
+}
+
+//Odd Number
+for (j = 1; j <= 20; j += 2) {
+    console.log(j);
+}
