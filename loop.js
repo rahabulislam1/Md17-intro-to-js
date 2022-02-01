2db7462f6d329d9fe8661828190c04805f163cbd
rahabulislam1 <rahabulislam1@gmail.com>
2022-02-01 12:37:21 +0600

    loop updated

--- loop.js
+++ loop.js
@@ -1,19 +1,28 @@
-var i = 1, j;
+var i, j;
 
 // while (i <= 10) {
 //     console.log('Count Num: ')
 //     console.log(i);
 //     i++;
 // }
+
 //even Number
-while (i <= 20) {
-    if (i % 2 == 0) {
-        console.log(i);
-    }
-    i++;
-}
+// while (i <= 20) {
+//     if (i % 2 == 0) {
+//         console.log(i);
+//     }
+//     i++;
+// }
 
 //Odd Number
-for (j = 1; j <= 20; j += 2) {
-    console.log(j);
+// for (j = 1; j <= 20; j += 2) {
+//     console.log(j);
+// }
+
+// var array = [34, 45, 12, 56, 67];
+var array = ['raju', 'dshd', 'dsjdn'];
+
+console.log(array.length);
+for (i = 0; i < array.length; i++) {
+    console.log(array[i]);
 }
